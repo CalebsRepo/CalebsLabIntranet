2766b6e1b766dcae84fd883e9df6fb4ac46ab4ab
jm <iamjim@hanmail.net>
2019-01-28 16:13:10 +0900

    planDetail.html, util.js

--- app/src/main/assets/js/util.js
+++ app/src/main/assets/js/util.js
@@ -1,9 +1,10 @@
 <script>
 
     /*DatePicker 사용 방법
-     1. 다음의 3개 링크, script를 head에 추가 .
+     1. 다음의 4개 링크, script를 head에 추가 .
      두개의 스크립트는 jquery-1.11.1.min.js와 jquery.mobile-1.4.5.min.js 사이에 추가
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="../js/util.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     2. input에 id 지정, input태그 하단에 아래 스크립트 추가
