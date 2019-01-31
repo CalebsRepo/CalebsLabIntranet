560169bc851d2b0bf03da417a3182dc11796699e
parkjinho82 <parkjinho82@gmail.com>
2019-01-31 13:29:13 +0900

    박진호 commit
    1. 공통 js 파일 분리
    2. jquery import 코드 common.js 에 추가
    3. onBackProcess 추가

---
+++ app/src/main/assets/js/common_util.js
@@ -0,0 +1,47 @@
+// 숫자체크
+function commonUtil_isNumber(data) {
+    var val = data;
+    var Num = "1234567890";
+    for (i=0; i<val.length; i++) {
+        if(Num.indexOf(val.substring(i,i+1))<0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 날짜체크
+function commonUtil_isDate(data) {
+    // '/'나 '-' 구분자 제거
+    var val = getRemoveFormat(data);
+
+    // 숫자, length 확인
+    if (isNumber(val, msg) && val.length == 8) {
+        var year = val.substring(0,4);
+        var month = val.substring(4,6);
+        var day = val.substring(6,8);
+
+        // 유효날짜 확인
+        if(checkDate(year,month,day,msg)){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+// 구분자 제거
+function commonUtil_getRemoveFormat(val) {
+    if(val.length == 10) {
+        var arrDate = new Array(3);
+        arrDate = val.split("/");
+        if(arrDate.length != 3) {
+            arrDate = val.split("-");
+        }
+        return arrDate[0] + arrDate[1] + arrDate[2];
+    } else {
+        return val;
+    }
+}
