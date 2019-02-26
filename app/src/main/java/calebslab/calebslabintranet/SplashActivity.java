837c508140315c9f643dbe0e14fa972fa2dbcc60
leeshva <leeshva093091@gmail.com>
2019-02-26 13:22:05 +0900

    splash activity 쓰레드 슬립 1초 설정

--- app/src/main/java/calebslab/calebslabintranet/SplashActivity.java
+++ app/src/main/java/calebslab/calebslabintranet/SplashActivity.java
@@ -11,6 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
