05bc444d34fc4d120cb862fea2b88ba98275d564
leeshva <leeshva093091@gmail.com>
2019-03-05 11:14:02 +0900

    메인엑티비티에서 휴가현황 엑셀 분리

--- app/src/main/java/calebslab/calebslabintranet/WebViewInterface.java
+++ app/src/main/java/calebslab/calebslabintranet/WebViewInterface.java
@@ -2,9 +2,37 @@ package calebslab.calebslabintranet;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class WebViewInterface {
@@ -12,6 +40,9 @@ public class WebViewInterface {
     private WebView mAppView;
     private Activity mContext;
     private static String userData;
+    /* 엑셀 파일 저장 관련 변수 */
+    Context context;
+    File xlsFile;
 
     /**
      * 생성자.
@@ -21,6 +52,7 @@ public class WebViewInterface {
     public WebViewInterface(Activity activity, WebView view) {
         mAppView = view;
         mContext = activity;
+
     }
 
 
@@ -51,6 +83,41 @@ public class WebViewInterface {
         sessionId = sessionId.substring(11);
 
         return sessionId;
+    }
+
+    /* 연차사용내역 엑셀 파일 생성 및 공유 */
+    @JavascriptInterface
+    public void saveExcel(final String result){
+        Toast.makeText(mContext.getApplicationContext(),"Excel 파일을 생성 중 입니다.",Toast.LENGTH_SHORT).show();
+
+        holidayConditionExcel he = new holidayConditionExcel();
+        Workbook workbook = he.makeExcel(result);
+
+        // 저장할 폴더 및 파일명
+        xlsFile = new File(mContext.getExternalFilesDir(null),"휴가사용내역.xls");
+
+        try{
+            FileOutputStream os = new FileOutputStream(xlsFile);
+            workbook.write(os);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) { // Android Nougat ( 7.0 ) and later
+            Uri uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider",xlsFile);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setDataAndType(uri, "application/excel");
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(Intent.EXTRA_STREAM,uri);
+            mContext.startActivity(Intent.createChooser(intent,"엑셀 내보내기"));
+
+        } else {
+            Uri uri = Uri.fromFile(xlsFile);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("application/excel");
+            intent.putExtra(Intent.EXTRA_STREAM,uri);
+            mContext.startActivity(Intent.createChooser(intent,"엑셀 내보내기"));
+        }
     }
 
 }
