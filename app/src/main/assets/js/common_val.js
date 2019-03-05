5d21cc5e53c6d9c7ac2af4f1fe3a68035c4118b8
skskrlwk <skskrlwk@naver.com>
2019-03-05 10:00:12 +0900

    commit_임규하

--- app/src/main/assets/js/common_val.js
+++ app/src/main/assets/js/common_val.js
@@ -1,9 +1,5 @@
 //전역변수 모음
-//var sndUrl = "http://calebslab1.cafe24.com/";	    //서버주소값
-//var sndUrl = "http://192.168.200.129:8080/Caleb/";	//집집 이거이거 서버주소값
-var sndUrl = "http://192.168.0.21:8080/Caleb/";	    //서버주소값
-//var sndUrl = "http://172.17.60.129:8080/Caleb/";	    //서버주소값
-
+var sndUrl = "http://calebslab1.cafe24.com/";	    //서버주소값
 
 var sessionInfo;
 var wasParams = {};  //was와 통실될 파라미터
